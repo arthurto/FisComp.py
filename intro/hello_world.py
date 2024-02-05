6804fae177ec4387a5ef7c9ae122ea53f9b09f20
arthurto <arthur.ps22009@gmail.com>
2024-02-05 11:34:43 -0300

    hello world

---
+++ intro/hello_world.py
@@ -0,0 +1,6 @@
+# Nesse programa vamos fazer o que todo
+# tutorial de python faz, que é printar
+# uma mensagem para mostrar como podemos
+# mostrar mensagens no terminal
+# <- a propósito isso aqui é um comentário
+print("Hello World, ou Olá mundo, já que estamos no Brasil!")
