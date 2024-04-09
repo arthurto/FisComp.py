02d38b1ac8dd9623d8d3e364a6d256c9678bd612
Arthur Pasq <arthur.ps22009@gmail.com>
2024-04-09 10:01:40 -0300

    adicionando referência pra estuo

---
+++ exemplos/varios_arquivos/gera_arquivo.py
@@ -0,0 +1,7 @@
+# O objetivo desse programa é criar uma função 
+# que tem como entrada dois arrays 
+# e gera como saída um arquivo no diretório de execução
+def gerar_arquivo(x,y):
+    pass
+
+# estudar=> "https://www.freecodecamp.org/portuguese/news/como-escrever-em-um-arquivo-em-python-open-read-append-e-outras-funcoes-de-manipulacao-explicadas/"
